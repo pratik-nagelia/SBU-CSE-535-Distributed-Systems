c923fb41a8224a218616bb0bea64b7939dc7d7ff
Shubham Agrawal <agrawalshubham1103@gmail.com>
2021-10-05 00:08:00 -0400

    Refactoring Safety Module

--- diemBFT/src/main/java/diem/component/Safety.java
+++ diemBFT/src/main/java/diem/component/Safety.java
@@ -29,7 +29,6 @@ public class Safety {
     return round + 1 == blockRound;
   }
 
-  // Need to Revisit
   private boolean safeToExtend(int blockRound, int qcRound, TimeoutCertificate tc) {
     return consecutive(blockRound, tc.round) && qcRound >= Collections.max(tc.tmoHighQCRounds);
   }
@@ -48,43 +47,57 @@ public class Safety {
     return consecutive(round, qcRound) || consecutive(round, tc.round);
   }
 
-  // Need to Revisit
+  //TODO
   private int commitStateIdCandidate(int blockRound, QuorumCertificate qc) {
     if (consecutive(blockRound, qc.voteInfo.round)) {
-      return Ledger.pendingState(0);
+      return Ledger.pendingState(qc.blockId);
     }
     return -1;
   }
 
-  public VoteMessage makeVote(Block b, TimeoutCertificate lastTc) {
-    int qcRound = b.quorumCertificate.voteInfo.round;
+  public VoteMessage makeVote(Block block, TimeoutCertificate lastTc) {
+    int qcRound = block.quorumCertificate.voteInfo.round;
 
-    // Add Valid Signature
-    if (safeToVote(b.round, qcRound, lastTc)) {
+    if (validSignature(block, lastTc) && safeToVote(block.round, qcRound, lastTc)) {
       updateHighestQcRound(qcRound);
-      increaseHighestVoteRound(b.round);
-      VoteInfo voteInfo = new VoteInfo(b.id, b.round, b.quorumCertificate.voteInfo.id, qcRound,
-          Ledger.pendingState(b.id));
+      increaseHighestVoteRound(block.round);
+
+      VoteInfo voteInfo = new VoteInfo(block.id, block.round, block.quorumCertificate.voteInfo.id,
+          qcRound,
+          Ledger.pendingState(block.id));
 
       // Need to Revisit
       LedgerCommitInfo ledgerCommitInfo = new LedgerCommitInfo(
-          commitStateIdCandidate(b.round, b.quorumCertificate), String.valueOf(voteInfo.id));
-      return new VoteMessage(voteInfo, ledgerCommitInfo, BlockTree.highCommitQC, 0,
-          new Signature());
+          commitStateIdCandidate(block.round, block.quorumCertificate), hash(voteInfo));
+
+      return new VoteMessage(voteInfo, ledgerCommitInfo, BlockTree.highCommitQC);
     }
     return null;
+  }
+
+  private String hash(VoteInfo voteInfo) {
+    return null;
+  }
+
+  //TODO
+  private boolean validSignature(Block b, TimeoutCertificate lastTc) {
+    return true;
   }
 
   public TimeoutInfo makeTimeout(int round, QuorumCertificate highestQc,
       TimeoutCertificate lastTc) {
     int qcRound = highestQc.voteInfo.round;
 
-    // Add Valid Signature
-    if (safeToTimeout(round, qcRound, lastTc)) {
+    if (validSignature(highestQc, lastTc) && safeToTimeout(round, qcRound, lastTc)) {
       increaseHighestVoteRound(round);
-      return new TimeoutInfo(round, highestQc, 0, new Signature());
+      return new TimeoutInfo(round, highestQc);
     }
     return null;
+  }
+
+  //TODO
+  private boolean validSignature(QuorumCertificate highQc, TimeoutCertificate lastTc) {
+    return true;
   }
 
 }
