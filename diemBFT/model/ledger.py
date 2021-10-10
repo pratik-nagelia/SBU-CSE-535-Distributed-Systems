9605c8aea68926c36cd82db8fd9cf7c77349c51b
Pratik Nagelia <pratik0204@gmail.com>
2021-10-10 03:49:26 -0400

    Model & Components in python (#3)
    * Python Start
    
    * Diem Java
    
    * add model mfiles
    
    * Gitignore
    
    * Block tree integrated
    
    * Committing safety module
    
    * Checked in PaceMaker
    
    Co-authored-by: Shubham Agrawal <agrawalshubham1103@gmail.com>
    Co-authored-by: spannu79 <sumeetpannu79@gmail.com>

---
+++ diemBFT/model/ledger.py
@@ -0,0 +1,12 @@
+class Ledger:
+    def speculate(self, prev_block_id, block_id, txns):
+        ledger_state_id = str(block_id)
+
+    def pending_state(self, block_id):
+        pass
+
+    def commit(self, block_id):
+        pass
+
+    def committed_block(self, block_id:
+        pass
