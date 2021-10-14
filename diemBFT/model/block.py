11b2fbb27abfd6a3fd9229338a35aa26157e2074
Pratik Nagelia <pratik0204@gmail.com>
2021-10-14 18:41:55 -0400

    QC Sent and received (#15)
    * Removed crap
    
    * Removed crap
    
    * Huge Refactorings and removings loads of crap
    
    * Code flow before process_vote blockTree
    
    Co-authored-by: Jarvis <pratik@gcp.com>

--- diemBFT/model/block.py
+++
@@ -1,7 +0,0 @@
-class Block:
-    def __init__(self, author, round, payload, quorum_certificate, block_id):
-        self.author = author
-        self.round = round
-        self.payload = payload
-        self.quorum_certificate = quorum_certificate
-        self.block_id = block_id
