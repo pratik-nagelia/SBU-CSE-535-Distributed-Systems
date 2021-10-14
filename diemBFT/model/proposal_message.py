ebad29a8221e80836b4b30866348b171f9acdc1a
Jarvis <pratik@gcp.com>
2021-10-14 06:21:36 +0000

    Removed crap

--- diemBFT/model/proposal_message.py
+++
@@ -1,7 +0,0 @@
-class ProposalMessage:
-    def __init__(self, block, last_round_tc, high_commit_qc):
-        self.block = block
-        self.last_round_tc = last_round_tc
-        self.high_commit_qc = high_commit_qc
-        # self.signature = signature
-        # self.sender = senders
