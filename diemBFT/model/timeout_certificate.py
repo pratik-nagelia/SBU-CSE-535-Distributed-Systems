57fd31591935187ef2157b03b2d68ada4b7fcc5e
Jarvis <pratik@gcp.com>
2021-10-14 06:31:18 +0000

    Removed crap

--- diemBFT/model/timeout_certificate.py
+++
@@ -1,5 +0,0 @@
-class TimeoutCertificate:
-    def __init__(self, round, tmo_high_qc_rounds, tmo_signatures):
-        self.round = round
-        self.tmo_high_qc_rounds = tmo_high_qc_rounds
-        self.tmo_signatures = tmo_signatures
