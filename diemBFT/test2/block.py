37dfbe4e90678fc96a0ede12a8e65e73177caa22
Jarvis <pratik@gcp.com>
2021-10-17 03:39:12 +0000

    Removed crap

--- diemBFT/test2/block.py
+++
@@ -1,9 +0,0 @@
-class Block:
-    def __init__(self, round, author, payload):
-        self.round = round
-        self.id = '123'
-        self.author = author
-        self.payload = payload
-
-    def __str__(self):
-        return 'Round: {}, Payload: {}'.format(self.round, self.payload)
--- diemBFT/test2/transaction.py
+++
@@ -1,6 +0,0 @@
-class Transaction:
-    def __init__(self, msg):
-        self.msg = msg
-
-    def __str__(self):
-        return self.msg
